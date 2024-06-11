d730e4a550b87d3070637491c7589ca73a2cc87a
Otavio Parraga <otavio.parraga@acad.pucrs.br>
2024-06-11 15:20:16 -0300

    Update Gradio link

--- api/index.py
+++ api/index.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-  return redirect("https://e185ef9a7d7c098d61.gradio.live")
+  return redirect("https://59b4cd8fc67661c1b2.gradio.live")
 
 if __name__ == "__main__":
   app.run()
